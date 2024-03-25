1341589048a56193f704e6e5fc010030b380f2ec
mcc1461 <mcoskuncelebi@gmail.com>
2024-03-25 11:36:26 +0300

    json file added

--- server-node/index.js
+++ server-node/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 app.use("/", (req, res) => {
     res.send("Hello from your SERVER!");
     });
+
+app.get('/api', (req, res) => {
+    const jsonFilePath = path.resolve('./', 'data.json');
+    const data = fs.readFileSync(jsonFilePath, "utf8", (err, data) => {
+        if (err) {
+            res.status(500).send("An error occurred while reading the file.");
+            return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+
+    });
+});
+
 
 app.listen(8005, () => {
     console.log("Server is running on port 8005");
